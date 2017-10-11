ba6abcdcf4c856d804fe953af34f88ebf0a6b3ff
Rahul Kalluri <rahkalluri@gmail.com>
2017-10-11 10:27:03 -0400

    change bunch of packages, near final?

--- debian-postinstall-script.sh
+++ debian-postinstall-script.sh
@@ -12,10 +12,21 @@
 sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/debian jessie-cran3/" >> /etc/apt/sources.list'
 sudo add-apt-repository -y ppa:marutter/rrutter
 
-# MongoDB enterprise 
+# MongoDB Community
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb http://repo.mongodb.com/apt/debian jessie/mongodb-enterprise/3.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-enterprise.list
+echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 
+#Cassandra by Apache
+echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list 
+curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add - 
+sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-key A278B781FE4B2BDA 
+
+
+# Synapse 
+sudo add-apt-repository ppa:synapse-core/ppa
+
+#Redshift instead of Flux 
+sudo add-apt-repository ppa:dobey/redshift-daily
 
 # Adds the CRAN GPG key, which is used to sign the R packages for security.
 sudo apt-key adv --keyserver subkeys.pgp.net --recv-key 381BA480
@@ -37,12 +48,12 @@ sudo apt-get -y --force-yes upgrade
 # install apps
 sudo apt-get -y --allow-unauthenticated install \
 	# tools
-	nano neovim git sublime-text-installer curl terminator firmware-iwlwifi neofetch tlp tlp-rdw tp-smapi-dkms acpi-call-dkms \
-	caffeine dpkg enpass fluxgui \
-	taskwarrior googler git-gui  \
+	nano neovim redshift git sublime-text-installer curl tilda firmware-iwlwifi neofetch tlp tlp-rdw tp-smapi-dkms acpi-call-dkms \
+	dpkg enpass synapse \
+	taskwarrior googler git-gui dselect tlp tlp-rdw tp-smapi-dkms acpi-call-dkms\
 	
 	# data science
-	mongodb-compass python3 python3-pip python3-neovim r-base r-base-dev openjdk-8-jdk mongodb-enterprise \
+	mongodb-compass python3 python3-pip python3-neovim r-base r-base-dev openjdk-8-jdk mongodb-org cassandra\
 		
 	# media
 	rtv spotify-client deluge vlc browser-plugin-vlc \
@@ -57,6 +68,7 @@ sudo pip3 install dbus-python pygobject
 
 # Install in R
 # install.packages(c("readr", "tidyverse", "tibble", "ISLR"))
+
 
 # prompt for a reboot
 clear
